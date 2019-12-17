a2645e72f610a26d32e90dea2ef164aff52cbd9a
yejue <18666996101@163.com>
2019-12-17 16:39:20 +0800

    更新了表关系模型的四张表

--- db_test/models.py
+++ db_test/models.py
@@ -11,3 +11,30 @@ class User(models.Model):
     def __str__(self):
         return 'id : {}, name: {}, city: {}'.format(self.id, self.name, self.city)
 
+
+# 学院表
+class College(models.Model):
+    c_id = models.AutoField(primary_key=True)
+    college_name = models.CharField(max_length=30)
+
+
+# 学生表
+class Student(models.Model):
+    s_id = models.IntegerField(primary_key=True)
+    student_name = models.CharField(max_length=20)
+    c_id = models.ForeignKey(College, on_delete=models.CASCADE)
+
+
+# 学生详情表
+class StudentDetail(models.Model):
+    sd_id = models.AutoField(primary_key=True)
+    student_age = models.IntegerField()
+    student_phone = models.IntegerField()
+    student = models.OneToOneField(Student, on_delete=models.CASCADE)
+
+
+# 课程表
+class Course(models.Model):
+    co_id = models.AutoField(primary_key=True)
+    course_name = models.CharField(max_length=30)
+    student = models.ManyToManyField(Student)
--- db_test/views.py
+++ db_test/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q
 
 # Create your views here.
 from .models import User
 
 
 def add(request):
-    # 方法一
-    temp = User(name='小明', city='珠海')
-    temp.save()
-    # 方法二
-    temp2 = User()
-    temp2.name = '小红'
-    temp2.city = '广州'
-    temp2.save()
-    # 方法三
-    User.objects.create(name='小亮', city='珠海')
-    # 方法四
-    User.objects.get_or_create(name='小李', city='重庆')
+    # # 方法一
+    # temp = User(name='小明', city='珠海')
+    # temp.save()
+    # # 方法二
+    # temp2 = User()
+    # temp2.name = '小红'
+    # temp2.city = '广州'
+    # temp2.save()
+    # # 方法三
+    User.objects.create(name='yejue', city='珠海')
+    User.objects.create(name='YEJUE', city='珠海')
+    # # 方法四
+    # User.objects.get_or_create(name='小李', city='重庆')
     return HttpResponse('添加成功')
 
 
@@ -30,9 +32,41 @@ def select(request):
     # # 查询所有
     # rs = User.objects.all()
     # 查询单个对象
-    rs = User.objects.get(id=1)
+    # rs = User.objects.get(id=1)
     # 查询满足条件的对象
     # rs = User.objects.filter(name='小明')
+    # 查询第一条数据
+    rs = User.objects.first()
+    # 查询最后一条数据
+    rs = User.objects.last()
+    # 排除查询，查询不满足条件的数据
+    rs = User.objects.exclude(name='小红')
+    # 查询排序
+    rs = User.objects.order_by('-id')
+    # 查询结果数据转化为字典
+    rs = User.objects.all().values()
+    # 查询结果计数
+    rs = User.objects.all().count()
+    # 条件查询
+    # exact 和 iexact
+    rs = User.objects.filter(name__exact='yejue')
+    rs = User.objects.filter(name__iexact='yejue')
+    # 以...开头，以...结尾
+    rs = User.objects.filter(name__startswith='小')
+    rs = User.objects.filter(name__endswith='明')
+    # 成员所属,in 查询
+    rs = User.objects.filter(name__in=['小明', '小红'])
+    # range 数字区间查询
+    rs = User.objects.filter(id__range=(12, 17))
+    # 大于 > ，大于等于 >= , 小于<， 小于等于 <=
+    rs = User.objects.filter(id__lt=15)   # 小于15
+    rs = User.objects.filter(id__lte=15)   # 小于等于15
+    rs = User.objects.filter(id__gt=15)   # 大于15
+    rs = User.objects.filter(id__gte=15)   # 大于等于15
+    # 使用or条件查询
+    rs = User.objects.filter(Q(name='小明') | Q(id=15))
+    print(rs.query)
+    #
     return render(request, 'db_test/add_del_select_update.html', context={
         'rs': rs
     })
